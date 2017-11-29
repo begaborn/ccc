d681e0997c03b71db51bdba1308fd09d8110823b
hyounsub.shin <begaborn@gmail.com>
2017-11-29 23:53:41 +0900

    記録時間単位を１分に変更

--- lib/util.rb
+++ lib/util.rb
@@ -10,12 +10,12 @@ def percent(num)
   num / 100.to_f
 end
 
-def log_file(str, min_unit = 15)
+def log_file(str, min_unit = 1)
   now = Time.now
   log_file_by(str, now, min_unit)
 end
 
-def pre_log_file(str, min_unit = 15)
+def pre_log_file(str, min_unit = 1)
   now = Time.now - (min_unit * 60)
   log_file_by(str, now, min_unit)
 end
@@ -23,11 +23,12 @@ end
 def log_file_by(str, now, min_unit)
   remain = now.min % min_unit
   m = "%02d" % (now.min - remain)
-  `mkdir -p log/#{str}`
-  "log/#{str}/#{now.year}#{now.month}#{now.day}#{now.hour}#{m}.log"
+  dir = "log/#{str}/#{now.year}#{now.month}#{now.day}"
+  `mkdir -p #{dir}`
+  "#{dir}/#{now.hour}#{m}.log"
 end
 
-def time_cut(now, min_unit = 15)
+def time_cut(now, min_unit = 1)
   remain = now.min % min_unit
   m = "%02d" % (now.min - remain)
   "#{now.year}/#{now.month}/#{now.day} #{now.hour}:#{m}"
