e811f9dc7c4f14c0c3c9624cbb2d6c2fa655953c
shinhyounsub <shinhyounsub@Shinui-MacBook-Pro.local>
2018-07-19 19:55:53 +0900

    Fix example for zaif

--- examples/zaif.rb
+++ examples/zaif.rb
@@ -7,13 +7,14 @@ Ccc.configure do |config|
 end
 
 Zaif.currencies.each do |c|
-  currency = Zaif.send(c)
+  currency = Zaif.send(c.to_sym)
   puts '-----------------------------------'
   puts "client = #{currency.client}"
   puts "config = #{currency.conf}"
   puts "currency_code = #{currency.currency_code}"
   puts "default_pair = #{currency.default_pair}"
   puts "currency_pair = #{currency.currency_pair}"
+  puts "min_amount = #{currency.min_amount}"
   puts "opening_price = #{currency.opening_price}"
   puts "closing_price = #{currency.closing_price}"
   puts "high_price = #{currency.high_price}"
@@ -22,9 +23,21 @@ Zaif.currencies.each do |c|
   puts "vpin = #{currency.vpin}"
   puts "buy_rate = #{currency.latest_buy_rate}"
   puts "sell_rate = #{currency.latest_sell_rate}"
+  puts "price_round_down = #{currency.price_round_down(12345.12345)}"
+  puts "amount_round_down = #{currency.amount_round_down(123.12345678901)}"
   puts "price = #{currency.price}"
+  puts "mid_price = #{currency.mid_price}"
+  puts "asks = #{currency.asks}"
+  puts "bids = #{currency.bids}"
+  puts "best_ask = #{currency.best_ask}"
+  puts "best_bid = #{currency.best_bid}"
   puts "volume = #{currency.volume24h}"
+  #amount = currency.buyable_amount(limit_cash: 1000, price: currency.price * 0.7)
+  #order_id = currency.stickily_buy amount, price: currency.price * 0.8
+  #puts "order_id = #{order_id}"
   currency.reload
+  #puts "my_orders = #{currency.my_orders}"
+  #puts "find_order = #{currency.find_order(order_id)}"
   puts "balance = #{currency.balance}"
   puts "locked_balance = #{currency.locked_balance}"
   puts "available_balance = #{currency.available_balance}"
@@ -38,14 +51,11 @@ Zaif.currencies.each do |c|
   puts "orderable_amount = #{currency.orderable_amount}"
   puts "sellable_amount = #{currency.sellable_amount}"
   puts "buyable_amount = #{currency.buyable_amount}"
+  currency.all_cancel
+  #res = currency.stickily_cancel order_id
+  #puts "canceled order_id = #{res}"
+  currency.reload
   puts "my_orders = #{currency.my_orders}"
-  puts "vwap = #{currency.vwap24h}"
-  puts "board = #{currency.depth}"
-  puts "mid_price = #{currency.mid_price}"
-  puts "asks = #{currency.asks}"
-  puts "bids = #{currency.bids}"
-  puts "best_ask = #{currency.best_ask}"
-  puts "best_bid = #{currency.best_bid}"
   puts ''
   sleep 1
 end
